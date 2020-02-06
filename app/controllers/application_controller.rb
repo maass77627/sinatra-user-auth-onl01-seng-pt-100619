7385c70ce821a29a8951bdbe4d684a91a8531c87
kelly r maass <kmaass77627@gmail.com>
2020-02-06 01:34:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/users/home'
     end
-    redirect '/sessions/login'
+   # redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
