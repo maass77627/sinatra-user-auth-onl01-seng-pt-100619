96023a4d1f3f0e8e8611438b0c0fabf9a38ef131
kelly r maass <kmaass77627@gmail.com>
2020-02-06 01:24:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,8 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
+    
+    puts params
 
     redirect '/users/home'
   end
@@ -45,7 +47,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
+binding.pry 
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
